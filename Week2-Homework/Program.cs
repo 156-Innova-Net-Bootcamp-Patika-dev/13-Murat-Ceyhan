7713a236555bcf6514ebae66a4e54f2a1202dd90
Murat Ceyhan <muratceyhan1133@gmail.com>
2022-01-20 23:28:32 +0300

    Database created.

---
+++ Week2-Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week2_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
